25ad31f3ec26570405e154608f647d25ebfe8fde
prajapatilalit <prajapatilalit150992@gmail.com>
2021-08-30 18:45:15 +0530

    create search bar component and setup youtube api

---
+++ yu-tube/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import SearchBar from "./SearchBar"
+
+
+function App() {
+  return (
+    <>
+      <div className="ui container">
+        <div className="ui seagment">
+          <SearchBar/>
+        </div>      
+      </div>
+    </>
+  );
+}
+
+export default App;
